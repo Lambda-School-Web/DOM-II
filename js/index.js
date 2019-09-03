5a2870bf39f2af31dbff402c04420b634d454663
jambis <imjaamees@gmail.com>
2019-09-03 18:13:42 -0500

    Added some event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,86 @@
 // Your code goes here
+
+//mouseenter and mouseleave image border
+const images = document.querySelectorAll("img");
+
+images.forEach(el =>
+  el.addEventListener("mouseenter", e => {
+    mouseEnterBorder(el);
+  })
+);
+
+function mouseEnterBorder(el) {
+  el.style.cssText = "border: 2px solid yellow; box-shadow: 5px 5px 3px grey";
+}
+
+images.forEach(el =>
+  el.addEventListener("mouseleave", e => {
+    mouseLeaveBorder(el);
+  })
+);
+
+function mouseLeaveBorder(el) {
+  el.style.cssText = "border: none";
+}
+
+//Wheel Zoom
+
+const containerElms = document.querySelectorAll(".container > *");
+let scale = 1;
+
+containerElms.forEach(el => (el.onwheel = zoom));
+
+function zoom(event) {
+  event.preventDefault();
+  scale += event.deltaY * -0.01;
+  event.toElement.style.transform = `scale(${scale})`;
+}
+
+//Reset scale on keypress of R
+const allElms = document.querySelectorAll("*");
+const bodyEl = document.querySelector("body");
+
+bodyEl.addEventListener("keypress", e => {
+  if (e.code == "KeyR") {
+    allElms.forEach(el => {
+      el.style.transform = `scale(1)`;
+    });
+  }
+});
+
+//Hover Links to Change background
+const navLinks = document.querySelectorAll("nav a");
+
+navLinks.forEach(el =>
+  el.addEventListener("mouseover", e => {
+    e.target.style.background = "green";
+  })
+);
+
+navLinks.forEach(el =>
+  el.addEventListener("mouseout", e => {
+    e.target.style.background = "white";
+  })
+);
+
+//Copy alert of paragraphs
+const pElms = document.querySelectorAll("p");
+
+function copyAlert(e) {
+  e.preventDefault();
+  e.clipboardData.setData("text/plain", "Shame on me for plagiarizing");
+  alert("No plagiarizing");
+}
+
+pElms.forEach(el => {
+  el.addEventListener("copy", copyAlert);
+});
+
+//Dragend Alert
+const headerImg = document.querySelector("#headerimg");
+console.log(headerImg);
+headerImg.addEventListener("dragend", e => {
+  alert(`Thanks for letting go of ${e.target.src}`);
+});
+
+//
